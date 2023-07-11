256aba06e1338f619d2e2a831de7fb41be0b2876
winona ivana <winonaivana@gmail.com>
2023-07-11 21:32:45 +0100

    add category folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookModule],
+  imports: [AuthModule, PrismaModule, BookModule, CategoryModule],
 })
 export class AppModule {}
