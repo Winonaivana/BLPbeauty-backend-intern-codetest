1a3b486015943bd85fba5e9c5b67bc5b437a25c7
winona ivana <winonaivana@gmail.com>
2023-07-12 18:00:52 +0100

    add user folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
 import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookModule, CategoryModule],
+  imports: [AuthModule, PrismaModule, BookModule, CategoryModule, UserModule],
 })
 export class AppModule {}
