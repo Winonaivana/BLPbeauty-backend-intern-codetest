4d768ecbeab76c50437315ccd1e205fed5149a34
winona ivana <winonaivana@gmail.com>
2023-07-12 14:12:07 +0100

    add find category by id endpoint

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CurrentUser } from 'src/decorators/user.decorator';
 import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
   @Get()
-  async showCategories(@CurrentUser() currentUser: User) {
-    return this.categoryService.showCategory(currentUser.id);
+  async findAllCategories(@CurrentUser() currentUser: User) {
+    return await this.categoryService.findAllCategory(currentUser.id);
+  }
+  @Get(':id')
+  async findCategoryById(
+    @CurrentUser() currentUser: User,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return await this.categoryService.findCategoryById(currentUser.id, id);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Category } from '@prisma/client';
+import { error } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CategoryService {
   constructor(private prisma: PrismaService) {}
 
-  async showCategory(userId: number): Promise<Category[]> {
+  async findAllCategory(userId: number): Promise<Category[]> {
     const Category = await this.prisma.category.findMany({
       where: { userId: userId },
     });
@@ -14,5 +19,24 @@ export class CategoryService {
       throw new UnauthorizedException('This book does not belong to you');
     }
     return Category;
+  }
+
+  async findCategoryById(userId: number, id: number) {
+    try {
+      const category = await this.prisma.category.findUnique({
+        where: { id: id },
+      });
+
+      if (category.userId !== userId) {
+        throw new UnauthorizedException('You do not own this category');
+      }
+      if (!category) {
+        throw new NotFoundException('Category not found');
+      }
+
+      return category;
+    } catch (error) {
+      throw error;
+    }
   }
 }
